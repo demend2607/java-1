c83a6dd78f9566f5611dc608109a9a129fcdc746
Vyacheslav <finder2607011@gmail.com>
2021-09-29 17:47:12 +1000

    xz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
+// touchstart
+// touchmove
+// touchenter
+// touchleave
+// touchcancel
 
 
 
 
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+
+        console.log('Start');
+    });
+});
