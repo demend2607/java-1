82bd27b6d290d6900d0d58c657b132a574933e13
Vyacheslav <finder2607011@gmail.com>
2021-10-08 11:15:30 +1000

    +work with div

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
-//! При написании действия необходимы круглые скобки в конце item();
+window.addEventListener('DOMContentLoaded', () => {
 
-"use strict";
+    const tabs = document.querySelectorAll('.tabheader__item'), // Используем: All - для получения всех элементов с данным тегом;
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
-const btns = document.querySelectorAll('button'),
-    wrapper = document.querySelector('.btn-block');
+    // Скрываем все блоки элемента .tabcontent
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-// console.log(btns[0].classList.length); // Узнаем количество классов у кнопок
-// console.log(btns[0].classList.item(1)); // item - Позволяет получить класс который стоит под определенным индексом
-// console.log(btns[1].classList.add('red')); // add - добавляет класс 
-// console.log(btns[0].classList.remove('blue')); // remove - удаляет класс
-// console.log(btns[0].classList.toggle('blue')); // toggle(переключать) - удаляет класс если он есть, и добавляет если его нету на странице
-// if (btns[1].classList.contains('red')) {
-//     console.log('red');
-// }
-
-btns[0].addEventListener('click', () => {
-    // if (!btns[1].classList.contains('red')) {
-    //     btns[1].classList.add('red');
-    // } else {
-    //     btns[1].classList.remove('red');
-    // }
-
-    btns[1].classList.toggle('red');
-});
-
-// console.log(btns[0].className);
-
-wrapper.addEventListener('click', (event) => {
-    if (event.target && event.target.matches("button.red")) {
-        console.log('Hello')
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active'); // обращаемся к html и убираем нужный класс у элемента
+        });
     }
+
+    // Показываем нужный нам элемент(.tabhead__item) на странице
+    function showTabContent(i = 0) { // 0 = i, 1-ый элемент;
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => { // item - каждый таб(элемент) который будем перебирать, i - номер элемента
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i); // i - номер совпавшего элемента
+                }
+
+            });
+
+        }
+    });
+
 });
-
-// btns.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//         console.log('Hello');
-//     });
-// });
-
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
