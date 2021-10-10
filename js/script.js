09669f8be81970e2846da8d70f27d899b0cddaed
Vyacheslav <finder2607011@gmail.com>
2021-10-10 14:13:38 +1000

    Timeout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-const timeId = setTimeout(function() {
-    console.log('Hello');
-}, 2000);
+const btn = document.querySelector('.btn');
+
+let timerId, // Назначаем глобальную переменную для работы локально
+    i = 0;
+
+function myAnimation() {
+    const elem = document.querySelector('.box'); // Название элемента на странице сайта
+    let pos = 0; // Позиция старта функции
+
+    const id = setInterval(frame, 10);
+
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+// function logger() { // Остановка интервала после определенного количества повторений
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+// Функция выполняется не мгновенно
+// Повторный вызов функции будет выполнятся после выполения предыдущей, а не через 500
+// let id = setTimeout(function log() { 
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// }, 500);
